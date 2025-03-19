9211f4178678e02721597da4f84bb3496f0f8a03
stickelt <stickelt@users.noreply.github.com>
2025-03-18 21:45:14 -0700

    switched to cursor with 3.7 sonnet thinking

--- CoreComponents/Extensions/ServiceCollectionExtensions.cs
+++ CoreComponents/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,26 @@
 using Stickelt.CoreComponents.Services;
 using Microsoft.Extensions.DependencyInjection;
 
+namespace CoreComponents.Extensions
+{
+    /// <summary>
+    /// Extension methods for IServiceCollection to register CoreComponents services
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds CoreComponents services to the service collection
+        /// </summary>
+        public static IServiceCollection AddCoreComponents(this IServiceCollection services)
+        {
+            // Register the ConfigurationService
+            services.AddScoped<IConfigurationService, ConfigurationService>();
+            
+            return services;
+        }
+    }
+}
+
 namespace Stickelt.CoreComponents.Extensions
 {
     /// <summary>
